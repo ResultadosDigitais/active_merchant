a2e364e304b0e584d41f0fb8a48b4065fee45d41
Tarciso Scariot <tarciso.scariot@rdstation.com>
2022-03-08 08:05:57 -0300

    fix ssl configure

--- lib/active_merchant/connection.rb
+++ lib/active_merchant/connection.rb
@@ -143,8 +143,6 @@ module ActiveMerchant
 
       if verify_peer
         http.verify_mode = OpenSSL::SSL::VERIFY_PEER
-        http.ca_file     = ca_file
-        http.ca_path     = ca_path
       else
         http.verify_mode = OpenSSL::SSL::VERIFY_NONE
       end
