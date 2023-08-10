d65d633da00f64d2321ca78f5c273f56aaef907f
willianzocolaurd <130781966+willianzocolaurd@users.noreply.github.com>
2023-08-10 18:31:34 -0300

    RMSTRATO-958 (#21)
    * feat:RMSTRATO-958: add encrypted cse card
    
    * feat:RMSTRATO-958: add cse-data to xml
    
    * chore:RMSTRATO-958: add csv value
    
    * chore:RMSTRATO-958: change attr
    
    * chore:RMSTRATO-958: change attr
    
    * test:RMSTRATO-958: add test

--- lib/active_merchant/billing/gateways/worldpay.rb
+++ lib/active_merchant/billing/gateways/worldpay.rb
@@ -243,6 +243,11 @@ module ActiveMerchant #:nodoc:
               xml.tag! 'TOKEN-SSL', token_scope_attribute(options) do
                 xml.tag! 'paymentTokenID', payment_method.payment_cryptogram
               end
+            elsif payment_method.is_a?(EncryptedCseCreditCard)
+              xml.tag! 'CSE-DATA' do
+                xml.tag! 'encryptedData', payment_method.encrypted_data
+                add_address(xml, (options[:billing_address] || options[:address]))
+              end
             else
               xml.tag! CARD_CODES[card_brand(payment_method)] do
                 xml.tag! 'cardNumber', payment_method.number
