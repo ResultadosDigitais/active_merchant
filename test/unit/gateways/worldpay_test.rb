8f578ec67ec77100620cd3c55e6f540d0bca69b8
RDErickKramer <erick.kramer@rdstation.com>
2026-07-29 09:15:15 -0300

    fix: update storedCredentials test to match expected XML structure in WorldpayTest

--- test/unit/gateways/worldpay_test.rb
+++ test/unit/gateways/worldpay_test.rb
@@ -593,7 +593,7 @@ class WorldpayTest < Test::Unit::TestCase
     response = stub_comms do
       @gateway.authorize(@amount, @credit_card, options)
     end.check_request do |endpoint, data, headers|
-      assert_match %r(<storedCredentials usage="FIRST"/>), data
+      assert_match %r(<storedCredentials usage="FIRST">\s*</storedCredentials>), data
     end.respond_with(successful_authorize_response)
     
     assert_success response
